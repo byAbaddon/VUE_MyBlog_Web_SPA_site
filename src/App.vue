262734be0c8daa3a80af988e6cef2751d9cae131
byAbaddon <kasiopess@abv.bg>
2022-05-05 00:41:27 +0300

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-<div>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
-  </div>
+  <v-row style="">
+    <v-col> <header-component /> </v-col>
+    <v-col>
+      <router-view />
+    </v-col>
+    <v-col></v-col>
+  </v-row>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import HeaderComponent from '@/components/HeaderComponent'
+
+export default {
+  components: {
+    HeaderComponent
+  },
+
+  data: () => ({})
 }
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss">
+body  {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,72 @@
+<template>
+ <div id="header">
+    <h1>MyBlog</h1>
+    <h2>Welcome <span>{{email}}!</span>!</h2>
+  <nav id="nav">
+      <ul>
+        <li><a href="#/logout">Logout</a></li>
+        <li><a href="#/">Home</a></li>
+        <li><a href="#/login">Login</a></li>
+        <li><a href="#/register">Register</a></li>
+      </ul>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#header {
+  background: #212121da;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 5px ;
+  position: absolute;
+  width: 100%;
+}
+
+#header h1 {
+  color: #fff;
+}
+
+#nav {
+  display: flex;
+}
+
+#nav ul {
+  display: flex;
+  list-style: none;
+}
+
+#nav li {
+  margin: 0 15px;
+}
+
+#nav a {
+  color: #fff;
+  text-decoration: none;
+  padding: 10px 15px;
+  text-align: center;
+  transition: 0.5s;
+}
+
+#nav a:hover {
+  background: #fff;
+  color: #212121;
+  padding: 6px 12px;
+}
+
+#header h2 {
+  color: #fff;
+  font-weight: 550;
+}
+
+#header h2 span {
+  color: rgb(255, 102, 0);
+}
+</style>
