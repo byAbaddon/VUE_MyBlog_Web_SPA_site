a58749e28dd90f48210d0cdb54ca4f3a3077bb4b
byAbaddon <kasiopess@abv.bg>
2022-05-18 01:43:54 +0300

    fix footer about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,216 +1,37 @@
 <template>
-  <div id="mainAbout">
-    <!-- body section -->
+<div class="main">
     <div class="pictures">
-      <img src="@/assets/images/about/4.png">
       <img src="@/assets/images/about/0.png">
       <img src="@/assets/images/about/1.png">
       <img src="@/assets/images/about/2.png">
       <img src="@/assets/images/about/3.png">
     </div>
-
-    <!-- footer -->
-    <div class="footer">
-      <div class="footer-container">
-        <div class="left-footer footer-widget-area">
-          <div id="block-48" class="widget-container widget_block">
-            <span id="travel-resources" class="uag-toc__heading-anchor"></span>
-            <h2 class="gb-headline gb-headline-281f0659 gb-headline-text">
-              Travel Resources
-            </h2>
-          </div>
-          <div id="block-41" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/travel/"
-                >How To Travel The World</a
-              >
-            </p>
-          </div>
-          <div id="block-40" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/best-travel-backpacks/"
-                >Picking A Travel Backpack</a
-              >
-            </p>
-          </div>
-          <div id="block-54" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/save-money-for-travel/"
-                >How To Save Money For Travel</a
-              >
-            </p>
-          </div>
-          <div id="block-55" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/travel-safety-tips/"
-                >Top Travel Safety Tips</a
-              >
-            </p>
-          </div>
-        </div>
-        <div class="middle-footer footer-widget-area">
-          <div id="block-49" class="widget-container widget_block">
-            <span id="social-media" class="uag-toc__heading-anchor"></span>
-            <h2 class="gb-headline gb-headline-6f48caff gb-headline-text">
-              Social Media
-            </h2>
-          </div>
-          <div id="block-23" class="widget-container widget_block">
-            <ul
-              class="
-                wp-container-5 wp-block-social-links
-                aligncenter
-                has-large-icon-size
-                items-justified-left
-              "
-            >
-              <li
-                class="
-                  wp-social-link wp-social-link-instagram wp-block-social-link
-                "
-              >
-                <a
-                  href="https://instagram.com/expertvagabond"
-                  aria-label="Instagram: https://instagram.com/expertvagabond"
-                  class="wp-block-social-link-anchor"
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-hidden="true"
-                    focusable="false"
-                  >
-                    <path
-                      d="M12,4.622c2.403,0,2.688,0.009,3.637,0.052c0.877,0.04,1.354,0.187,1.671,0.31c0.42,0.163,0.72,0.358,1.035,0.673 c0.315,0.315,0.51,0.615,0.673,1.035c0.123,0.317,0.27,0.794,0.31,1.671c0.043,0.949,0.052,1.234,0.052,3.637 s-0.009,2.688-0.052,3.637c-0.04,0.877-0.187,1.354-0.31,1.671c-0.163,0.42-0.358,0.72-0.673,1.035 c-0.315,0.315-0.615,0.51-1.035,0.673c-0.317,0.123-0.794,0.27-1.671,0.31c-0.949,0.043-1.233,0.052-3.637,0.052 s-2.688-0.009-3.637-0.052c-0.877-0.04-1.354-0.187-1.671-0.31c-0.42-0.163-0.72-0.358-1.035-0.673 c-0.315-0.315-0.51-0.615-0.673-1.035c-0.123-0.317-0.27-0.794-0.31-1.671C4.631,14.688,4.622,14.403,4.622,12 s0.009-2.688,0.052-3.637c0.04-0.877,0.187-1.354,0.31-1.671c0.163-0.42,0.358-0.72,0.673-1.035 c0.315-0.315,0.615-0.51,1.035-0.673c0.317-0.123,0.794-0.27,1.671-0.31C9.312,4.631,9.597,4.622,12,4.622 M12,3 C9.556,3,9.249,3.01,8.289,3.054C7.331,3.098,6.677,3.25,6.105,3.472C5.513,3.702,5.011,4.01,4.511,4.511 c-0.5,0.5-0.808,1.002-1.038,1.594C3.25,6.677,3.098,7.331,3.054,8.289C3.01,9.249,3,9.556,3,12c0,2.444,0.01,2.751,0.054,3.711 c0.044,0.958,0.196,1.612,0.418,2.185c0.23,0.592,0.538,1.094,1.038,1.594c0.5,0.5,1.002,0.808,1.594,1.038 c0.572,0.222,1.227,0.375,2.185,0.418C9.249,20.99,9.556,21,12,21s2.751-0.01,3.711-0.054c0.958-0.044,1.612-0.196,2.185-0.418 c0.592-0.23,1.094-0.538,1.594-1.038c0.5-0.5,0.808-1.002,1.038-1.594c0.222-0.572,0.375-1.227,0.418-2.185 C20.99,14.751,21,14.444,21,12s-0.01-2.751-0.054-3.711c-0.044-0.958-0.196-1.612-0.418-2.185c-0.23-0.592-0.538-1.094-1.038-1.594 c-0.5-0.5-1.002-0.808-1.594-1.038c-0.572-0.222-1.227-0.375-2.185-0.418C14.751,3.01,14.444,3,12,3L12,3z M12,7.378 c-2.552,0-4.622,2.069-4.622,4.622S9.448,16.622,12,16.622s4.622-2.069,4.622-4.622S14.552,7.378,12,7.378z M12,15 c-1.657,0-3-1.343-3-3s1.343-3,3-3s3,1.343,3,3S13.657,15,12,15z M16.804,6.116c-0.596,0-1.08,0.484-1.08,1.08 s0.484,1.08,1.08,1.08c0.596,0,1.08-0.484,1.08-1.08S17.401,6.116,16.804,6.116z"
-                    ></path></svg
-                ></a>
-              </li>
-              <li
-                class="
-                  wp-social-link wp-social-link-youtube wp-block-social-link
-                "
-              >
-                <a
-                  href="https://youtube.com/expertvagabond"
-                  aria-label="YouTube: https://youtube.com/expertvagabond"
-                  class="wp-block-social-link-anchor"
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-hidden="true"
-                    focusable="false"
-                  >
-                    <path
-                      d="M21.8,8.001c0,0-0.195-1.378-0.795-1.985c-0.76-0.797-1.613-0.801-2.004-0.847c-2.799-0.202-6.997-0.202-6.997-0.202 h-0.009c0,0-4.198,0-6.997,0.202C4.608,5.216,3.756,5.22,2.995,6.016C2.395,6.623,2.2,8.001,2.2,8.001S2,9.62,2,11.238v1.517 c0,1.618,0.2,3.237,0.2,3.237s0.195,1.378,0.795,1.985c0.761,0.797,1.76,0.771,2.205,0.855c1.6,0.153,6.8,0.201,6.8,0.201 s4.203-0.006,7.001-0.209c0.391-0.047,1.243-0.051,2.004-0.847c0.6-0.607,0.795-1.985,0.795-1.985s0.2-1.618,0.2-3.237v-1.517 C22,9.62,21.8,8.001,21.8,8.001z M9.935,14.594l-0.001-5.62l5.404,2.82L9.935,14.594z"
-                    ></path></svg
-                ></a>
-              </li>
-              <li
-                class="
-                  wp-social-link wp-social-link-facebook wp-block-social-link
-                "
-              >
-                <a
-                  href="https://facebook.com/expertvagabond"
-                  aria-label="Facebook: https://facebook.com/expertvagabond"
-                  class="wp-block-social-link-anchor"
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-hidden="true"
-                    focusable="false"
-                  >
-                    <path
-                      d="M12 2C6.5 2 2 6.5 2 12c0 5 3.7 9.1 8.4 9.9v-7H7.9V12h2.5V9.8c0-2.5 1.5-3.9 3.8-3.9 1.1 0 2.2.2 2.2.2v2.5h-1.3c-1.2 0-1.6.8-1.6 1.6V12h2.8l-.4 2.9h-2.3v7C18.3 21.1 22 17 22 12c0-5.5-4.5-10-10-10z"
-                    ></path></svg
-                ></a>
-              </li>
-              <li
-                class="
-                  wp-social-link wp-social-link-twitter wp-block-social-link
-                "
-              >
-                <a
-                  href="https://twitter.com/expertvagabond"
-                  aria-label="Twitter: https://twitter.com/expertvagabond"
-                  class="wp-block-social-link-anchor"
-                >
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-hidden="true"
-                    focusable="false"
-                  >
-                    <path
-                      d="M22.23,5.924c-0.736,0.326-1.527,0.547-2.357,0.646c0.847-0.508,1.498-1.312,1.804-2.27 c-0.793,0.47-1.671,0.812-2.606,0.996C18.324,4.498,17.257,4,16.077,4c-2.266,0-4.103,1.837-4.103,4.103 c0,0.322,0.036,0.635,0.106,0.935C8.67,8.867,5.647,7.234,3.623,4.751C3.27,5.357,3.067,6.062,3.067,6.814 c0,1.424,0.724,2.679,1.825,3.415c-0.673-0.021-1.305-0.206-1.859-0.513c0,0.017,0,0.034,0,0.052c0,1.988,1.414,3.647,3.292,4.023 c-0.344,0.094-0.707,0.144-1.081,0.144c-0.264,0-0.521-0.026-0.772-0.074c0.522,1.63,2.038,2.816,3.833,2.85 c-1.404,1.1-3.174,1.756-5.096,1.756c-0.331,0-0.658-0.019-0.979-0.057c1.816,1.164,3.973,1.843,6.29,1.843 c7.547,0,11.675-6.252,11.675-11.675c0-0.178-0.004-0.355-0.012-0.531C20.985,7.47,21.68,6.747,22.23,5.924z"
-                    ></path></svg
-                ></a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="right-footer footer-widget-area">
-          <div id="block-50" class="widget-container widget_block">
-            <span
-              id="travel-photography"
-              class="uag-toc__heading-anchor"
-            ></span>
-            <h2 class="gb-headline gb-headline-158a9271 gb-headline-text">
-              Travel Photography
-            </h2>
-          </div>
-          <div id="block-51" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/travel-photography-tips/"
-                >How To Improve Your Photos</a
-              >
-            </p>
-          </div>
-          <div id="block-56" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/best-travel-camera/"
-                >Recommended Travel Cameras</a
-              >
-            </p>
-          </div>
-          <div id="block-57" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a href="https://expertvagabond.com/best-photography-courses/"
-                >Best Online Photography Classes</a
-              >
-            </p>
-          </div>
-          <div id="block-53" class="widget-container widget_block widget_text">
-            <p class="has-text-align-center">
-              <a
-                href="https://expertvagabond.com/prints"
-                target="_blank"
-                rel="noreferrer noopener"
-                >Travel Photography Prints</a
-              >
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    
+   <div class="before-footer">
+       <h2>Get connected with us on social networks!</h2>
+    </div> 
+   <div class="footer">
+    <a href="#" class="fa fa-facebook"></a>
+    <a href="#" class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-google"></a>
+    <a href="#" class="fa fa-linkedin"></a>
+    <a href="#" class="fa fa-youtube"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    <a href="#" class="fa fa-pinterest"></a>
+    <a href="#" class="fa fa-snapchat-ghost"></a>
+    <a href="#" class="fa fa-skype"></a>
+    <a href="#" class="fa fa-android"></a>
+    <a href="#" class="fa fa-dribbble"></a>
+    <a href="#" class="fa fa-vimeo"></a>
+    <a href="#" class="fa fa-tumblr"></a>
+    <a href="#" class="fa fa-vine"></a>
+    <a href="#" class="fa fa-foursquare"></a>
+    <a href="#" class="fa fa-stumbleupon"></a>
+    <a href="#" class="fa fa-flickr"></a>
+    <a href="#" class="fa fa-yahoo"></a>
+    <a href="#" class="fa fa-reddit"></a>
+    <a href="#" class="fa fa-rss"></a>
+   </div>
   </div>
 </template>
 
@@ -222,10 +43,10 @@ export default {
 </script>
 
 
-
-
 <style scoped>
-
+.main{
+  margin-top: 3em;
+}
 
 img{
   height: 40em;
@@ -240,20 +61,139 @@ img{
 
 
 .footer {
-  font-size: 0.875rem;
-  padding: 50px 20px;
-  color: #555;
   background-color: #201d1d;
-
-  color: rgb(255, 250, 250);
+  display: flex;
+  justify-content: space-evenly;
+  padding-bottom: 4em;
+ 
 }
 
-.footer-container {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin: -10px;
-  padding: 20px;
+.before-footer h2{
+  background-color: #201d1d;
+  color: white;
+  margin-bottom: 0;
+  padding: 1.4em;
+}
+
+
+
+.fa {
+  padding: 10px;
+  font-size: 20px;
+  width: 20px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
+  border-radius: 50%;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-pinterest {
+  background: #cb2027;
+  color: white;
+}
+
+.fa-snapchat-ghost {
+  background: #fffc00;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+
+.fa-skype {
+  background: #00aff0;
+  color: white;
+}
+
+.fa-android {
+  background: #a4c639;
+  color: white;
+}
+
+.fa-dribbble {
+  background: #ea4c89;
+  color: white;
+}
+
+.fa-vimeo {
+  background: #45bbff;
+  color: white;
+}
+
+.fa-tumblr {
+  background: #2c4762;
+  color: white;
+}
+
+.fa-vine {
+  background: #00b489;
+  color: white;
+}
+
+.fa-foursquare {
+  background: #45bbff;
+  color: white;
+}
+
+.fa-stumbleupon {
+  background: #eb4924;
+  color: white;
+}
+
+.fa-flickr {
+  background: #f40083;
+  color: white;
+}
+
+.fa-yahoo {
+  background: #430297;
+  color: white;
+}
+
+.fa-soundcloud {
+  background: #ff5500;
+  color: white;
+}
+
+.fa-reddit {
+  background: #ff5700;
+  color: white;
+}
+
+.fa-rss {
+  background: #ff6600;
+  color: white;
 }
 </style>
